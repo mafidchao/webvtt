2e5c4b544fcc0c56869f0633d54cb6d146de9153
Ralph Giles <giles@mozilla.com>
2012-02-17 16:15:04 -0800

    Add MPL 2.0 license headers to the source files.
    We may want something more liberal than this, but
    the MPL at least allows distribution.

--- webvtt.h
+++ webvtt.h
@@ -1,5 +1,10 @@
 /* WebVTT parser
    Copyright 2011 Mozilla Foundation
+
+   This Source Code Form is subject to the terms of the Mozilla
+   Public License, v. 2.0. If a copy of the MPL was not distributed
+   with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
  */
 
 #ifndef _WEBVTT_H_
