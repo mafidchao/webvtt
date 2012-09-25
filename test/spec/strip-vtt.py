addc901370bf10c09ea28a54302be8100529d0e9
Rick Eyre <rick.eyre@hotmail.com>
2012-09-25 19:54:11 -0400

    Modified Makefile to now run strip-vtt.py before run-tests-js.py. Makefile check-js now runs run-tests-js.py on the ./objdir/spec directory. Created directory crawler functions to plugin to Jordans vtt stripper.

---
+++ test/spec/strip-vtt.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+Loops through the directory path passed to it in sys.argv[1], finds any .test files, strips the .vtt file from it, and
+saves it to a mirrored directory structure under webvtt/objdir/spec
+"""
+
+import os
+import sys
+
+def findTestFiles(root):
+
+	dirList = os.listdir(root)
+
+	for d in dirList:
+				
+		dir_path = os.path.join(root, d)
+
+		if os.path.isdir(dir_path):
+			findTestFiles(dir_path)
+		elif os.path.splitext(dir_path)[1] == ".test":
+			#call jordan function here. pass dir_path as file path
+			print dir_path
+
+def main():
+	findTestFiles(os.path.realpath(sys.argv[1]))
+
+if __name__ == '__main__':
+	status = main()
+	sys.exit(status)
