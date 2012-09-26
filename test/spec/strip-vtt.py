859330d90df6b3bdd1fafdddc8079ab22cd2ae2b
Rick Eyre <rick.eyre@hotmail.com>
2012-09-25 20:45:44 -0400

    Modified findTestFiles to use list in order to plugin to strip function easier.

--- test/spec/strip-vtt.py
+++ test/spec/strip-vtt.py
@@ -15,21 +15,27 @@ def stripVTT(file_path):
   # Rip the VTT
   vtt_file = file_data[file_data.find('*/\n') + 3:]
 
-def findTestFiles(root):
-
+def findTestFiles(root, fileList):
+	#List the directory
 	dirList = os.listdir(root)
 
 	for d in dirList:
-				
+		#Join the folder and/or file name that has been listed to the root path
 		dir_path = os.path.join(root, d)
-
+		#If the path is a directory than recursivley walk the directory tree
 		if os.path.isdir(dir_path):
-			findTestFiles(dir_path)
+			findTestFiles(dir_path, fileList)
+		#If the file path is for a .test file than append it to the file list
 		elif os.path.splitext(dir_path)[1] == ".test":
-			stripVTT(dir_path)
+			fileList.append(dir_path)
 
 def main():
-	findTestFiles(os.path.realpath(sys.argv[1]))
+	#List that will contain the file paths of the .test files
+	fileList = list()
+	findTestFiles(os.path.realpath(sys.argv[1]), fileList)
+
+	for f in fileList:
+		pass
 
 if __name__ == '__main__':
 	status = main()
