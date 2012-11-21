f35d11c3f618f93a155b338c79099872648c12c3
Caitlin Potter <snowball@defpixel.com>
2012-11-21 17:24:16 -0500

    Some work on the cue settings unit tests, the 'vertical' setting 'good' tests are both implemented

--- test/unit/csvertical_unittest.cpp
+++ test/unit/csvertical_unittest.cpp
@@ -9,7 +9,7 @@ From http://dev.w3.org/html5/webvtt/#webvtt-vertical-text-cue-setting (09/28/201
 	2. A U+003A COLON character (:).
 	3. One of the following strings: "rl", "lr". 
 */
-TEST(WebVTTCueSettingVertical,ParseRL)
+TEST(WebVTTCueSettingVertical,RL)
 {
 	try
 	{
@@ -18,6 +18,32 @@ TEST(WebVTTCueSettingVertical,ParseRL)
 		
 		const Cue &cue = parser.getCue(0);
 		ASSERT_EQ(cue.isVerticalRightToLeft(),true);
+	}
+	catch( std::exception e )
+	{
+		// Something broke.
+		ASSERT_TRUE(false);
+	}
+}
+
+/*
+Verifies that the parser correctly parses a "vertical" key, followed by U+003A ':',
+followed by 'lr' (indicating that the text be positioned vertically, and grows towards the right)
+
+From http://dev.w3.org/html5/webvtt/#webvtt-vertical-text-cue-setting (09/28/2012):
+	1. The string "vertical".
+	2. A U+003A COLON character (:).
+	3. One of the following strings: "rl", "lr". 
+*/
+TEST(WebVTTCueSettingVertical,LR)
+{
+	try
+	{
+		ItemStorageParser parser("tc4002-cue_setting_vertical_02_good.vtt");
+		parser.parse();
+		
+		const Cue &cue = parser.getCue(0);
+		ASSERT_EQ(cue.isVerticalLeftToRight(),true);
 	}
 	catch( std::exception e )
 	{
