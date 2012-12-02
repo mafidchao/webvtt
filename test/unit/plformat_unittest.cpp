c419d585a41ea97b12190686776a7fe3425f45f3
mafidchao <mdafidchao@.learn.senecac.on.ca>
2012-12-02 14:56:24 -0500

    Added unit tests for CR/CRLF line terminators

--- test/unit/plformat_unittest.cpp
+++ test/unit/plformat_unittest.cpp
@@ -70,3 +70,60 @@ TEST_F(PayloadFormat,DISABLED_MultilineMultipleCueTextTag)
 	node = node->child( 0 )->toInternalNode();
 	ASSERT_EQ( Node::Bold, node->kind() );
 }
+
+/*
+ * Verifies that cue text separated by a CR line terminator is parsed correctly.
+ * From http://dev.w3.org/html5/webvtt/#webvtt-cue-text (12/02/2012)
+ *	Cue text consists of one or more cue text components optionally separated by a single line terminator which can be: 
+ *		1. CR (U+000D)
+ *		2. LF (U+000A)
+ *		3. CRLF pair
+ */
+TEST_F(PayloadFormat,MultilineBasicCueTextCR)
+{
+	loadVtt( "payload/payload-format/multiline-basic-cue-text-cr.vtt" );
+	ASSERT_EQ( Node::Text, getHeadOfCue( 0 )->child( 0 )->kind() );
+}
+
+/*
+ * Verifies that cue text separated by a CRLF line terminator is parsed correctly.
+ * From http://dev.w3.org/html5/webvtt/#webvtt-cue-text (12/02/2012)
+ *	Cue text consists of one or more cue text components optionally separated by a single line terminator which can be: 
+ *		1. CR (U+000D)
+ *		2. LF (U+000A)
+ *		3. CRLF pair
+ */
+TEST_F(PayloadFormat,MultilineBasicCueTextCRLF)
+{
+	loadVtt( "payload/payload-format/multiline-basic-cue-text-crlf.vtt" );
+	ASSERT_EQ( Node::Text, getHeadOfCue( 0 )->child( 0 )->kind() );
+}
+
+/* Verifies that cue text with a malformed line terminator is still parsed correctly.
+ * From http://dev.w3.org/html5/webvtt/#webvtt-parser-algorithm (12/02/2012)
+ * The WebVTT parser algorithm is as follows:
+ * [...] 50. Bad cue: Discard cue.
+ */
+TEST_F(PayloadFormat,MultilineBasicCueTextExtraLine)
+{
+	loadVtt( "payload/payload-format/multiline-extra-line-terminator.vtt" );
+	ASSERT_EQ( Node::Text, getHeadOfCue( 0 )->child( 0 )->kind() );
+}
+
+/* Verifies that cue text with a span tag (bold) and an escape character is parsed correctly.
+ * From http://dev.w3.org/html5/webvtt/#webvtt-cue-text (12/02/2012)
+ * WebVTT cue text consists of zero or more WebVTT cue components, in any order,
+ * each optionally separated from the next by a WebVTT line terminator.
+ */
+TEST_F(PayloadFormat,MultilineBasicCueTextExtraLine)
+{
+	loadVtt( "payload/payload-format/component-and-escape-together.vtt" );
+	const InternalNode *head = getHeadOfCue( 0 );
+	const TextNode *node = getHeadOfCue( 0 )->child( 0 )->toTextNode();
+	//ASSERT_TRUE( head->childCount() == 3 );
+	ASSERT_EQ( head->childCount(), 3 );
+	ASSERT_EQ( Node::Bold, head->child( 1 )->kind() );
+	
+	String expectedText = String( (const byte *)"&", 1 );
+	ASSERT_EQ( expectedText.text(), node->content().text() );
+}
