bcfadaea7f7d8387d43cd208af407dbd41eb6def
Jordan Raffoul <raffoul.jordan@gmail.com>
2012-12-02 19:59:04 -0500

    remove segfaulting test

--- test/unit/plrubytag_unittest.cpp
+++ test/unit/plrubytag_unittest.cpp
@@ -179,30 +179,3 @@ TEST_F(PayloadRubyTag,DISABLED_NoRubyText)
   ASSERT_EQ( Node::RubyText, head1->child( 1 )->toInternalNode()->child( 1 )->kind() );
   ASSERT_TRUE( head1->child( 1 )->toInternalNode()->child( 1 )->toInternalNode()->childCount() == 0 );
 }
-
-/**
- * Verifies that the parser correctly parses a ruby tag with an optional WEBVTT Line Terminator
- * after a an </rt> tag.
- *
- * http://dev.w3.org/html5/webvtt/#webvtt-cue-ruby-span (11/25/2012):
- * A WebVTT cue ruby span consists of the following components, in the order given:
- * 1. A WebVTT cue span start tag "ruby" that disallows an annotation.
- * 2. One or more occurrences of the following group of components, in the order given:
- *  1. WebVTT cue internal text, representing the ruby base.
- *  2. A WebVTT cue span start tag "rt" that disallows an annotation.
- *  3. A WebVTT cue ruby text span: WebVTT cue internal text, representing the ruby text component 
- *     of the ruby annotation.
- *  4. A WebVTT cue span end tag "rt". If this is the last occurrence of this group of components in
- *     the WebVTT cue ruby span, then this last end tag string may be omitted.
- * 3. If the last end tag string was not omitted: Optionally, a WebVTT line terminator.
- * 4. If the last end tag string was not omitted: Zero or more U+0020 SPACE characters or U+0009
- *    CHARACTER TABULATION (tab) characters, each optionally followed by a WebVTT line terminator.
- * 5. A WebVTT cue span end tag "ruby".
- */
-TEST_F(PayloadRubyTag,DISABLED_OptionalLineTerminator)
-{
-  /*
-   * Seg Fault
-   */
-  loadVtt( "payload/ruby-tag/optional-line-terminator.vtt");
-}
