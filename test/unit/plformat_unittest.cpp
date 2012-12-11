0323a1ef211b9d3441fc6707330973a4ccd45514
mafidchao <mdafidchao@.learn.senecac.on.ca>
2012-12-10 23:21:13 -0500

    Added more unit tests

--- test/unit/plformat_unittest.cpp
+++ test/unit/plformat_unittest.cpp
@@ -22,6 +22,17 @@ TEST_F(PayloadFormat,DISABLED_MultipleCueTextTag)
 }
 
 /*
+ * Verifies that multiple cue components can be put in one line, one after
+ * the other.
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-components
+ */
+TEST_F(PayloadFormat,DISABLED_MultipleCueTextTagTogether)
+{
+	loadVtt( "payload/payload-format/multiple-cue-tag-together.vtt", 1 );
+	ASSERT_TRUE( getHeadOfCue( 0 )->toInternalNode()->childCount() == 5 );
+}
+
+/*
  * Verifies that a cue text span with multiple lines will work.
  * From http://dev.w3.org/html5/webvtt/#webvtt-cue-text (11/27/2012)
  *	Cue text text consists of one or more cue text components optionally separated by a single line terminator which can be: 
@@ -47,6 +58,79 @@ TEST_F(PayloadFormat,DISABLED_MultilineCueText)
 {
 	loadVtt( "payload/payload-format/multiline-cue-text.vtt", 1 );
 	ASSERT_TRUE( getHeadOfCue( 0 )->toInternalNode()->childCount() == 4 );
+}
+
+/*
+ * Verifies that cue text with single carriage return characters will be parsed.
+ * From http://dev.w3.org/html5/webvtt/#webvtt-cue-text (11/27/2012)
+ *	Cue text text consists of one or more cue text components optionally separated by a single line terminator which can be: 
+ *		1. CR (U+000D)
+ *		2. LF (U+000A)
+ *		3. CRLF pair
+ */
+TEST_F(PayloadFormat,DISABLED_MultilineCueTextCR)
+{
+	loadVtt( "payload/payload-format/multiline-cue-text-cr.vtt", 1 );
+	ASSERT_TRUE( getHeadOfCue( 0 )->toInternalNode()->childCount() == 4 );
+}
+
+/*
+ * Verifies that cue text with single CRLF pair characters will be parsed.
+ * From http://dev.w3.org/html5/webvtt/#webvtt-cue-text (11/27/2012)
+ *	Cue text text consists of one or more cue text components optionally separated by a single line terminator which can be: 
+ *		1. CR (U+000D)
+ *		2. LF (U+000A)
+ *		3. CRLF pair
+ */
+TEST_F(PayloadFormat,DISABLED_MultilineCueTextCRLF)
+{
+	loadVtt( "payload/payload-format/multiline-cue-text-crlf.vtt", 1 );
+	ASSERT_TRUE( getHeadOfCue( 0 )->toInternalNode()->childCount() == 4 );
+}
+
+/*
+ * Verifies that a file with payload text containing a line feed in between the text 
+ * will finish the parsing attempt gracefully.
+ * From http://dev.w3.org/html5/webvtt/#parsing (12/10/2012):
+ *
+ * ...
+ * 50. Bad cue: Discard cue.
+ * 51. Bad cue loop: If position is past the end of input, then jump to the step labeled end. 
+ */
+TEST_F(PayloadFormat,DISABLED_MultilineCueTextExtraNewline)
+{
+	loadVtt( "payload/payload-format/multiline-cue-text-extra-newline.vtt", 1 );
+	ASSERT_TRUE( getHeadOfCue( 0 )->toInternalNode()->childCount() == 2 );
+}
+
+/*
+ * Verifies that a file with payload text containing a carriage return in between the text 
+ * will finish the parsing attempt gracefully.
+ * From http://dev.w3.org/html5/webvtt/#parsing (12/10/2012):
+ *
+ * ...
+ * 50. Bad cue: Discard cue.
+ * 51. Bad cue loop: If position is past the end of input, then jump to the step labeled end. 
+ */
+TEST_F(PayloadFormat,DISABLED_MultilineCueTextExtraNewlineCR)
+{
+	loadVtt( "payload/payload-format/multiline-cue-text-extra-newline-cr.vtt", 1 );
+	ASSERT_TRUE( getHeadOfCue( 0 )->toInternalNode()->childCount() == 2 );
+}
+
+/*
+ * Verifies that a file with payload text containing a CRLF pair in between the text 
+ * will finish the parsing attempt gracefully.
+ * From http://dev.w3.org/html5/webvtt/#parsing (12/10/2012):
+ *
+ * ...
+ * 50. Bad cue: Discard cue.
+ * 51. Bad cue loop: If position is past the end of input, then jump to the step labeled end. 
+ */
+TEST_F(PayloadFormat,DISABLED_MultilineCueTextExtraNewlineCRLF)
+{
+	loadVtt( "payload/payload-format/multiline-cue-text-extra-newline-crlf.vtt", 1 );
+	ASSERT_TRUE( getHeadOfCue( 0 )->toInternalNode()->childCount() == 2 );
 }
 
 /*
